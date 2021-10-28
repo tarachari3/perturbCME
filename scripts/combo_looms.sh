4c84cab563cad06110a4c50df5d4afd55a83d6d3
tarachari3 <tarachari3@gmail.com>
2021-10-28 15:52:24 -0700

    Add loom scripts

---
+++ scripts/combo_looms.sh
@@ -0,0 +1,6 @@
+
+import loompy
+
+# ds = loompy.connect("Cortex.loom")
+# loompy.combine(files, key="gene_id")
+# d = ds.view[:,np.isin(ds.ca.barcode,['AAACCTGAGAGATGAG']).nonzero()[0]] #How to subset
