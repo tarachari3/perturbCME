bd1e3d1162a090a2b3e06e3dceca0afeb8333988
tarachari3 <tarachari3@gmail.com>
2022-05-17 17:14:56 -0700

    Add Norman analysis

--- scripts/concatLoomsNorman.py
+++ scripts/concatLoomsNorman.py
@@ -67,8 +67,10 @@ remain = [[i] for i in remain if i not in pair]
 #All conditions/paired conditions
 assigns = remain + [pair]
 
-#Comment out to not split up controls
-assigns = [['NegCtrl10_NegCtrl0__NegCtrl10_NegCtrl0'],['NegCtrl11_NegCtrl0__NegCtrl11_NegCtrl0'],['NegCtrl1_NegCtrl0__NegCtrl1_NegCtrl0']]
+#Comment out below to not split up controls/samples
+#assigns = [['NegCtrl10_NegCtrl0__NegCtrl10_NegCtrl0'],['NegCtrl11_NegCtrl0__NegCtrl11_NegCtrl0'],['NegCtrl1_NegCtrl0__NegCtrl1_NegCtrl0']]
+assigns = [['NegCtrl0_ETS2__NegCtrl0_ETS2','ETS2_NegCtrl0__ETS2_NegCtrl0'],
+['NegCtrl0_CNN1__NegCtrl0_CNN1','CNN1_NegCtrl0__CNN1_NegCtrl0']]
 
 #For each drug condition get cell barcodes/counts and save loom file
 for a in assigns:
