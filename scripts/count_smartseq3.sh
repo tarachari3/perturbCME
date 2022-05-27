bb882e237f81cbd7a20d7e1f85b8ede1fbbdf525
tarachari3 <tarachari3@gmail.com>
2022-05-26 18:27:31 -0700

    Add smartseq analysis

--- scripts/count_smartseq3.sh
+++ scripts/count_smartseq3.sh
@@ -7,9 +7,10 @@ kb count --verbose \
 -x SMARTSEQ3 \
 -o ../counts/smartseq3_fibro/kbOut/ \
 -t 30 -m 30G \
+-w /home/tchari/metadata/fibro_smartseq/whitelist.txt \
 -c1 /home/ggorin/ref/refdata-gex-mm10-2020-A/kallisto/cdna_t2c.txt \
 -c2 /home/ggorin/ref/refdata-gex-mm10-2020-A/kallisto/intron_t2c.txt \
---workflow lamanno --overwrite --loom \
+--workflow lamanno --overwrite --dry-run \
 /home/tchari/counts/smartseq3_fibro/fastq/Smartseq3.Fibroblasts.NovaSeq.I1.fastq.gz \
 /home/tchari/counts/smartseq3_fibro/fastq/Smartseq3.Fibroblasts.NovaSeq.I2.fastq.gz \
 /home/tchari/counts/smartseq3_fibro/fastq/Smartseq3.Fibroblasts.NovaSeq.R1.fastq.gz  \
