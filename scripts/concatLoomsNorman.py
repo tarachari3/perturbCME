307bf69e284ce39c669dbb2c0c9b7ec5dadbff6b
tarachari3 <tarachari3@gmail.com>
2022-10-05 15:43:29 -0700

    Add monod notebook for Norman

--- scripts/concatLoomsNorman.py
+++ scripts/concatLoomsNorman.py
@@ -26,20 +26,25 @@ unspliced = []
 sNames = []
 uNames = []
 
+filtName = '/counts_filtered'#'/counts_unfiltered'
+
 for i in range(len(samples)):
 	samp = samples[i]
 
-	s = sio.mmread(data_path+samp+'/counts_unfiltered/spliced.mtx')
+	s = sio.mmread(data_path+samp+filtName+'/spliced.mtx')
 	spliced += [s]
 
-	u = sio.mmread(data_path+samp+'/counts_unfiltered/unspliced.mtx')
+	u = sio.mmread(data_path+samp+filtName+'/unspliced.mtx')
 	unspliced += [u]
 
-	sNames += [x+'-'+str(i+1) for x in list(pd.read_csv(data_path+samp+'/counts_unfiltered/spliced.barcodes.txt',header=None)[0])]
-	uNames += [x+'-'+str(i+1) for x in list(pd.read_csv(data_path+samp+'/counts_unfiltered/unspliced.barcodes.txt',header=None)[0])]
+	sNames += [x+'-'+str(i+1) for x in list(pd.read_csv(data_path+samp+filtName+'/spliced.barcodes.txt',header=None)[0])]
+	uNames += [x+'-'+str(i+1) for x in list(pd.read_csv(data_path+samp+filtName+'/unspliced.barcodes.txt',header=None)[0])]
 
 #geneNames = np.array(list(pd.read_csv(data_path+samp+'/counts_unfiltered/spliced.genes.txt',header=None)[0]))
-ds = loompy.connect(data_path+samp+'/counts_unfiltered/adata.loom')
+
+ds = loompy.connect(data_path+samp+filtName+'/adata.loom')
+#ds = loompy.connect(data_path+samp+'/counts_filtered/adata.loom') #Only if ok with losing barcodes
+
 geneNames = ds.ra['gene_name'] #np.array(list(pd.read_csv(data_path+samp+'/counts_unfiltered/spliced.genes.txt',header=None)[0]))
 ds.close()
 
@@ -54,60 +59,72 @@ print('Barcodes:')
 print(len(sNames))
 print(len(uNames))
 
+fname = out_path+'allcrispr.loom'
+saveAdata = anndata.AnnData(
+			X=allS,
+			layers={
+				'spliced': allS,
+				'unspliced': allU
+			},
+			obs=pd.DataFrame({'barcode': np.array(sNames)},index=np.array(sNames)),
+			var=pd.DataFrame({'gene_name': np.array(geneNames)},index=np.array(geneNames))
+		)
+
+saveAdata.write_loom(fname)
 
 #Read in scanpy metadata
 
-meta = pd.read_csv(meta_path+'norman_GSE133344_filtered_cell_identities.csv')
+# meta = pd.read_csv(meta_path+'norman_GSE133344_filtered_cell_identities.csv')
 
-pair = ['NegCtrl10_NegCtrl0__NegCtrl10_NegCtrl0','NegCtrl11_NegCtrl0__NegCtrl11_NegCtrl0','NegCtrl0_NegCtrl0__NegCtrl0_NegCtrl0']
+# pair = ['NegCtrl10_NegCtrl0__NegCtrl10_NegCtrl0','NegCtrl11_NegCtrl0__NegCtrl11_NegCtrl0','NegCtrl0_NegCtrl0__NegCtrl0_NegCtrl0']
 
-remain = np.unique(meta.guide_identity)
-remain = [[i] for i in remain if i not in pair]
+# remain = np.unique(meta.guide_identity)
+# remain = [[i] for i in remain if i not in pair]
 
-#All conditions/paired conditions
-assigns = remain + [pair]
+# #All conditions/paired conditions
+# assigns = remain + [pair]
 
-#Comment out below to not split up controls/samples
-assigns = [['NegCtrl10_NegCtrl0__NegCtrl10_NegCtrl0','NegCtrl11_NegCtrl0__NegCtrl11_NegCtrl0','NegCtrl0_NegCtrl0__NegCtrl0_NegCtrl0']]
-#assigns = [['NegCtrl0_ETS2__NegCtrl0_ETS2','ETS2_NegCtrl0__ETS2_NegCtrl0'],
-#['NegCtrl0_CNN1__NegCtrl0_CNN1','CNN1_NegCtrl0__CNN1_NegCtrl0']]
+# #Comment out below to not split up controls/samples
+# assigns = [['NegCtrl10_NegCtrl0__NegCtrl10_NegCtrl0','NegCtrl11_NegCtrl0__NegCtrl11_NegCtrl0','NegCtrl0_NegCtrl0__NegCtrl0_NegCtrl0']]
+# #assigns = [['NegCtrl0_ETS2__NegCtrl0_ETS2','ETS2_NegCtrl0__ETS2_NegCtrl0'],
+# #['NegCtrl0_CNN1__NegCtrl0_CNN1','CNN1_NegCtrl0__CNN1_NegCtrl0']]
 
-#For each drug condition get cell barcodes/counts and save loom file
-for a in assigns:
+# #For each drug condition get cell barcodes/counts and save loom file
+# for a in assigns:
 
-	barcodes = list(meta['cell_barcode'][meta['guide_identity'].isin(a)])
+# 	barcodes = list(meta['cell_barcode'][meta['guide_identity'].isin(a)])
 
-	sfilt = [sNames.index(x) for x in barcodes]
-	print('Condition: ', a)
-	print('Sp. Filt:', len(sfilt))
-	ufilt = [uNames.index(x) for x in barcodes]
+# 	sfilt = [sNames.index(x) for x in barcodes]
+# 	print('Condition: ', a)
+# 	print('Sp. Filt:', len(sfilt))
+# 	ufilt = [uNames.index(x) for x in barcodes]
 
 
-	subS = allS.tocsr()[sfilt,:]
-	subU = allU.tocsr()[ufilt,:]
+# 	subS = allS.tocsr()[sfilt,:]
+# 	subU = allU.tocsr()[ufilt,:]
 
-	print(subS.shape)
-	print(subU.shape)
+# 	print(subS.shape)
+# 	print(subU.shape)
 
-	if subS.shape[0] > 0:
-		#Save loom files in data_path
-		names = '_'.join(a)
-		fname = out_path+'crispr'+names+'.loom'
+# 	if subS.shape[0] > 0:
+# 		#Save loom files in data_path
+# 		names = '_'.join(a)
+# 		fname = out_path+'crispr'+names+'.loom'
 
-		#row_attrs = { "Gene": geneNames } #genes
-		#col_attrs = { "Barcode": np.array(barcodes) } #cells
+# 		#row_attrs = { "Gene": geneNames } #genes
+# 		#col_attrs = { "Barcode": np.array(barcodes) } #cells
 
-		retAdata = anndata.AnnData(
-			X=subS,
-			layers={
-				'spliced': subS,
-				'unspliced': subU
-			},
-			obs=pd.DataFrame({'Barcode': np.array(barcodes)},index=np.array(barcodes)),
-			var=pd.DataFrame({'Gene': np.array(geneNames)},index=np.array(geneNames))
-		)
+# 		retAdata = anndata.AnnData(
+# 			X=subS,
+# 			layers={
+# 				'spliced': subS,
+# 				'unspliced': subU
+# 			},
+# 			obs=pd.DataFrame({'Barcode': np.array(barcodes)},index=np.array(barcodes)),
+# 			var=pd.DataFrame({'Gene': np.array(geneNames)},index=np.array(geneNames))
+# 		)
 
-		retAdata.write_loom(fname)
+# 		retAdata.write_loom(fname)
 
 
 
