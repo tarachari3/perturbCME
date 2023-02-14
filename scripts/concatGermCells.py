450b420460f7fe9680ac5b880616074b42ec6876
tarachari3 <tarachari3@gmail.com>
2023-02-14 15:44:50 -0800

    Add germ cell processing

---
+++ scripts/concatGermCells.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+from scipy import sparse
+import scanpy as sc
+import anndata
+import loompy
+import scipy.io as sio
+
+data_path = '/home/tchari/counts/germcell_splicing/'
+meta_path = '/home/tchari/metadata/'
+out_path = "/home/tchari/counts/germcell_splicing/loom/"
+
+
+testS = pd.read_csv(data_path+'GSE136220_raw_counts_matrix_pgcs_no_adrenal.csv',index_col=0)
+testU = pd.read_csv(data_path+'GSE136220_raw_counts_matrix_pgcs_no_adrenal_unspliced.csv',index_col=0)
+ids = [s1.split(':')[0] for s1 in testS.index]
+
+meta = pd.DataFrame()
+meta['identity'] = ids
+meta['cell_barcode'] = list(testS.index)
+meta.to_csv(meta_path+'germCell_meta.csv',index=None)
+
+print(meta.head())
+
+geneNames = testS.columns.values.tolist() #Check
+
+S = testS.to_numpy()
+U = testU.to_numpy()
+
+fname = out_path+'allconds.loom'
+
+print('Making Loom')
+#Make loom of U/S
+retAdata = anndata.AnnData(
+			X=sparse.csr_matrix(S),
+			layers={
+				'spliced': sparse.csr_matrix(S),
+				'unspliced': sparse.csr_matrix(U),
+			},
+			obs=pd.DataFrame({'barcode': np.array(list(testS.index))},index=np.array(list(testS.index))),
+			var=pd.DataFrame({'gene_name': np.array(geneNames)},index=np.array(geneNames))
+		)
+
+retAdata.write_loom(fname)
+
