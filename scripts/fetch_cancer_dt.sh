72d034399170e15cbebe3a237b6a7c4752967fcb
tarachari3 <tarachari3@gmail.com>
2023-09-29 18:36:38 -0700

    Run cancer ctrl and neg ctrl meKMeans

--- scripts/fetch_cancer_dt.sh
+++ scripts/fetch_cancer_dt.sh
@@ -2,4 +2,5 @@
 #Get all fastqs
 #See https://github.com/ncbi/sra-tools/wiki/02.-Installing-SRA-Toolkit for updated sra tools
 
-prefetch SRR9833038 --max-size 30000000000 -O ./ && fasterq-dump --include-technical --split-files SRR9833038
+prefetch SRR9833038 --max-size 30000000000 -O ./ && fasterq-dump --include-technical --split-files SRR9833038 && \
+prefetch SRR9833037 --max-size 60000000000 -O ./ &&  fasterq-dump --include-technical --split-files SRR9833037 
